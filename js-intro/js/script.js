e759f77bdae0aa622cc43b9ec4ff1bedc12f84a7
Isak <aslundisak@gmail.com>
2018-01-25 17:47:41 +0100

    Initial commit, litet demo för vad java script kan göra

---
+++ js-intro/js/script.js
@@ -0,0 +1,91 @@
+// Ändra text i en paragraf
+var paragraph = document.querySelector('.name');
+
+paragraph.addEventListener('click', updateName);
+
+function updateName() {
+  var name = prompt('Skriv in ett nytt namn:');
+  paragraph.innerHTML = 'Namn: ' + name;
+}
+
+
+//Lyssna på när någon klickar på knappen
+var timesClicked = 0;
+var h3 = document.querySelector('h3');
+
+function addClick(){
+    timesClicked += 1;
+    h3.innerHTML = 'Du har tryckt på knappen ' + timesClicked + 'ggr'; 
+}
+  
+var click_btn = document.querySelector('#click-btn');
+click_btn.addEventListener('click', addClick)
+
+
+//Spel
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber)
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+var guessCount = 1;
+var resetButton;
+guessField.focus();
+
+function checkGuess() {
+    var userGuess = Number(guessField.value);
+    if(guessCount === 1) {
+      guesses.textContent = 'Previous guesses: ';
+    }
+    guesses.textContent += userGuess + ' ';
+    if(userGuess === randomNumber) {
+      lastResult.textContent = 'Congratulations! You got it right!';
+      lastResult.style.backgroundColor = 'green';
+      lowOrHi.textContent = '';
+      setGameOver();
+    } else if(guessCount === 10) {
+      lastResult.textContent = '!!!GAME OVER!!!';
+      lowOrHi.textContent = '';
+      setGameOver();
+    } else {
+      lastResult.textContent = 'Wrong!';
+      lastResult.style.backgroundColor = 'red';
+      if(userGuess < randomNumber) {
+        lowOrHi.textContent = 'Last guess was too low!';
+      } else if(userGuess > randomNumber) {
+        lowOrHi.textContent = 'Last guess was too high!';
+      }
+    }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.querySelector('.game').appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+    var resetParas = document.querySelectorAll('.resultParas p');
+    for(var i = 0 ; i < resetParas.length ; i++) {
+      resetParas[i].textContent = '';
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    lastResult.style.backgroundColor = 'white';
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber)
+  }
