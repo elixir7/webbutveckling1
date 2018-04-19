2f867212bf57838d794d89a67a543d3883f26a64
Isak Åslund <aslundisak@gmail.com>
2018-04-19 17:55:35 +0200

    Added folder for assignment and dokument for showing some principles of JS

---
+++ js-lektion2/js/script.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var buyPrice = 5000;
+var sellPrice = 6200;
+
+var profitMargin = 1.25;
+
+var answer = sellPrice > buyPrice * profitMargin
+
+console.log("Janne will make money on the pryl: " + answer)
+*/
+
+
+
+/*
+function windChillEffect(t, v){
+	// t = air temperature (C)
+	// v = wind speed (m/s)
+	var vPow = Math.pow(v, 0.16);
+    var feelsLike = 13.12 + 0.6215*t - 11.37*vPow + 0.3965*t*vPow;
+    return 'Feels like ' + feelsLike
+} 
+*/
+
+/*
+function shouldJanneBuy(buyPrice, sellPrice, profitMargin){	
+    var profitable = sellPrice > buyPrice * profitMargin;
+    return profitable;
+} 
+*/
+
+/*
+var button = document.getElementById("button");
+button.addEventListener("click", calcArea)
+
+var outputField = document.getElementById("area")
+
+function calcArea() {
+    var height = document.getElementById("height").value;
+    var base = document.getElementById("base").value;
+
+    var area = (base * height) / 2;
+    console.log(height)
+    console.log(base)
+    console.log(area)
+
+    outputField.innerHTML = " " + area + " cm^2";
+
+}
+*/
+
---
+++ js-miniuppgift/js/script.js
@@ -0,0 +1,50 @@
+// Här skriver du Java Script koden för uppgifterna
+// Kom ihåg att du får titta på presentationerna på classroom och ta hjälp av internet.
+// Tips: Kolla på w3schools.com för syntax, exempel m.m
+
+// Uppgift 1
+/*
+Du ska göra ett js program som beräknar arean av en triangel givet basen och höjden.
+Area_triangel = (bas * höjd) / 2
+
+    1. Skapa en variabel för höjden och en för basen.
+    2. Tilldela dem godtyckliga värden.
+    3. Skapa en variabel för arean och beräkna den.
+    4. Skriv ut svaret i konsolen.
+*/
+
+
+
+// Uppgift 2
+/*
+Ta hjälp av en funktion för att kunna beräkna olika areor givet olika baser och höjder.
+
+    1. Skapa en funktion som tar bas och höjd som parametrar
+    2. Funktionen ska retunera arean
+    3. Använd funktionen för att beräkna en area och skriv ut det i konsolen.
+*/
+
+
+
+
+// Uppgift 3
+/*
+Nu skall du skriva ut vad arean blir på hemsidan istället för konsolen.
+
+    1. Skapa en HTML tagg i HTML koden och se till att du kan referera till den m.h.a t.ex. Klass eller id
+    2. Använd funktionen från uppg. 2 för beräkning av area
+    3. Skriv ut arean i din html tagg
+*/
+
+
+
+// Brütal
+// Det finns lite hjälp i html koden som du kan kommentera ut och använda
+/*
+Nu skall du hämta basen och längden från hemsidan som användaren skrivit in. 
+Sedan skall arean beräknas och skrivas ut på hemsidan m.h.a en funktion.
+
+    1. Studera och använd den ut kommenterade koden i html dokumentet.
+    2. Skapa en funktion som körs när användaren klickar på knappen.
+    3. Funktionen skall hämta datan från fälten på hemsidan, beräkna arean och sedan skriva ut den på sidan.
+*/
